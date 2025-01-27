d7240fb5959468432e38cc09afa6830bbcc9e3d0
Cenk Uluisik <cenk.uluisik@googlemail.com>
2025-01-27 01:55:13 +0100

    opi5b defconfig

---
+++ shell.nix
@@ -0,0 +1,25 @@
+{
+  pkgs ? import <nixpkgs> { },
+}:
+
+pkgs.mkShell rec {
+  buildInputs = with pkgs; [
+
+    (python3.withPackages (
+      p: with p; [
+        setuptools
+        pyelftools
+      ]
+    ))
+
+    swig
+    ncurses
+    gnumake
+    bison
+    flex
+    openssl
+    bc
+
+  ];
+  LD_LIBRARY_PATH = buildInputs;
+}
